033aacf2ce4ef63aa56493c8658421228ec5b71e
vargasmichael <119553981+vargasmichael@users.noreply.github.com>
2023-09-05 14:23:24 -0600

    initialize first books

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Booklist = () => {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/41d4KgoiJhL._SX311_BO1,204,203,200_.jpg"
+    alt="Things We Left Behing"
+  />
 );
+const Title = () => <h2>Things We Left Behind</h2>;
+const Author = () => <h4>Lucy Score</h4>;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(<Booklist />);
